54d71f37d9ba18f4a9b05352a4472405a0aaedf5
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2025-03-22 13:31:42 +0530

    deploy

--- Smart-Balance/vite.config.js
+++ Smart-Balance/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Smart-Balance/',  // Ensure this matches your subfolder in Netlify
+  base: '/Smart-Balance/',  // Adjust base path
+  build: {
+    outDir: 'Smart-Balance/dist'  // Ensure the output folder is correct
+  }
 })
