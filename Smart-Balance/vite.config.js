e8afe04a4b4e940394347bee736fd84bbe3dae81
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2025-03-22 13:37:56 +0530

    deploy

--- Smart-Balance/vite.config.js
+++ Smart-Balance/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Smart-Balance/',  // Adjust base path
-  build: {
-    outDir: 'Smart-Balance/dist'  // Ensure the output folder is correct
-  }
 })
